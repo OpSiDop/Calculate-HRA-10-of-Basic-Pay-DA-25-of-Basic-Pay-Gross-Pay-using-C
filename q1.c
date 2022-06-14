ada7f77d0820600f1f109923e34e1d3b8d0cefb7
OpSiDop <sraghaw2002@gmail.com>
2022-06-14 17:25:34 +0530

    C program

---
+++ q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	float Basic, HRA, DA, Gross_Salary;
+  	
+	printf("\n Please Enter the Basic Salary of an Employee  :  ");
+  	scanf("%f", &Basic);
+  
+  
+  	
+  		HRA = (Basic * 10) / 100;// or HRA = Basic * (10 / 100)
+		DA = (Basic * 25) / 100; 
+		
+  	
+  	
+	Gross_Salary = Basic + HRA + DA;
+	printf("\nGross Salary of this Employee  =  %.2f", Gross_Salary);
+	printf("\nDA is : %.2f",DA);
+	printf("\nHRA is : %.2f",HRA);
+	
+  	return 0;
+}
